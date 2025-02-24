7f7b44ea49b4468ba5ba7cfa85430427047176c8
ShabiGardezi <shabigardezi51214@gmail.com>
2025-02-25 01:34:59 +0500

    rate limiter

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const UserController = require("../controllers/userController");
-
+const apiLimiter = require("../middlewares/rateLimiter");
 const router = express.Router();
 
-router.get("/get-platform-users", UserController.getPlatformUsers);
-router.delete("/delete-platform-user/:id", UserController.deletePlatformUser);
-router.get("/get-admin-dashboard-users", UserController.getAdminDashboardUsers);
-router.put("/update-dashboard-user/:id", UserController.updateDashboardUser);
-router.delete("/delete-dashboard-user/:id", UserController.deleteDashboardUser);
-
+router.get("/get-platform-users", apiLimiter, UserController.getPlatformUsers);
+router.delete("/delete-platform-user/:id", apiLimiter, UserController.deletePlatformUser);
+router.get("/get-admin-dashboard-users", apiLimiter, UserController.getAdminDashboardUsers);
+router.put("/update-dashboard-user/:id", apiLimiter, UserController.updateDashboardUser);
+router.delete("/delete-dashboard-user/:id", apiLimiter, UserController.deleteDashboardUser);
 
 module.exports = router;
