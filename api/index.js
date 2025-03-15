2a1d7a72438818177204885874156fcf02fa75a9
hafizmudassar88 <h4hafiz@gmail.com>
2025-03-15 16:44:42 -0400

    updated

--- api/index.js
+++ api/index.js
@@ -1,48 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const routes = require("./routes");
-// const errorHandler = require("./middleware/errorHandler");
-// const { PORT } = require("./config/environment");
-// const connectDB = require("./config/dbconnection");
-
-// const app = express();
-
-// const corsOptions = {
-//   origin: "*",
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-// };
-
-// app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
-
-// app.use(express.json());
-
-// app.use(routes);
-// app.use(errorHandler);
-
-// app.get("/", (req, res) => res.send("Express on Vercel"));
-
-// // Start the server and connect to the database
-// const startServer = async () => {
-//   try {
-//     await connectDB();
-//     app.listen(PORT, () => {
-//       console.log(`Server running on http://localhost:${PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Failed to connect to MongoDB", error);
-//     process.exit(1);
-//   }
-// };
-
-// startServer();
-
-// module.exports = app;
-
-// //
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
@@ -52,28 +7,16 @@ const connectDB = require("./config/dbconnection");
 
 const app = express();
 
-// Allow all origins
 const corsOptions = {
-  origin: "*", // Allow all origins
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-  credentials: false, // Set to true if using cookies or authorization headers
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
-
-// Handle preflight requests
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-    return res.sendStatus(204);
-  }
-  next();
-});
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 
